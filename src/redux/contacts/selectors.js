54fce454cb5aa253603505963d5b6f6189fe968d
wombatstar <toxaf13@gmail.com>
2022-12-29 15:52:21 +0100

    2,1

--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -1,4 +1,3 @@
-import { createSelector } from "@reduxjs/toolkit";
 
 export const selectLoading = state => state.tasks.loading;
 
@@ -7,14 +6,3 @@ export const selectFilter = state => state.tasks.filter;
 export const selectItems = state => state.tasks.items;
 
 
-//export const selectContacts = createSelector(
-//   [selectItems, selectFilter],
-//   (items, filter) => {
-//     if (filter === '') return items;
-//     const filteredArray = items.filter(item => {
-//       const filtered = filter.toLowerCase();
-//       return item.name.toLowerCase().includes(filtered);
-//     });
-//     return filteredArray;
-//   }
-// );
